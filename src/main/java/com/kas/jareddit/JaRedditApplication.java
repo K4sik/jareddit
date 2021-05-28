403c2cf2816c50fb82cf1d56310837c1989e6aeb
K4sik <kasiks597@gmail.com>
2021-05-28 11:44:32 +0300

    Initial commit

---
+++ src/main/java/com/kas/jareddit/JaRedditApplication.java
@@ -0,0 +1,13 @@
+package com.kas.jareddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaRedditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaRedditApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kas/jareddit/JaRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kas.jareddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaRedditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
