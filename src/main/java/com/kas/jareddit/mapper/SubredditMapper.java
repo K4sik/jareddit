8d697872f729ce7c2ee72b14d2640ae31c5c3d31
K4sik <kasiks597@gmail.com>
2021-05-31 16:45:34 +0300

    Subreddit Mapstruct

---
+++ src/main/java/com/kas/jareddit/mapper/SubredditMapper.java
@@ -0,0 +1,25 @@
+package com.kas.jareddit.mapper;
+
+import com.kas.jareddit.dto.SubredditDto;
+import com.kas.jareddit.model.Post;
+import com.kas.jareddit.model.Subreddit;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post> numberOfPosts) {
+        return numberOfPosts.size();
+    }
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subreddit);
+}
--- src/main/java/com/kas/jareddit/service/SubredditService.java
+++ src/main/java/com/kas/jareddit/service/SubredditService.java
@@ -1,7 +1,9 @@
 package com.kas.jareddit.service;
 
 import com.kas.jareddit.dto.SubredditDto;
+import com.kas.jareddit.exception.JARedditException;
 import com.kas.jareddit.exception.SubredditNotFoundException;
+import com.kas.jareddit.mapper.SubredditMapper;
 import com.kas.jareddit.model.Subreddit;
 import com.kas.jareddit.repository.SubredditRepository;
 import lombok.AllArgsConstructor;
@@ -19,13 +21,27 @@ public class SubredditService {
 
     private final SubredditRepository subredditRepository;
     private final AuthService authService;
+    private final SubredditMapper subredditMapper;
+
+//    @Transactional
+//    public SubredditDto save(SubredditDto subredditDto) {
+//        Subreddit save = subredditRepository.save(subredditMapper.mapDtoToSubreddit(subredditDto));
+//        subredditDto.setId(save.getId());
+//        return subredditDto;
+//    }
 
     @Transactional(readOnly = true)
     public List<SubredditDto> getAll() {
         return subredditRepository.findAll()
                 .stream()
-                .map(this::mapToDto)
+                .map(subredditMapper::mapSubredditToDto)
                 .collect(toList());
+    }
+
+    public SubredditDto getSubreddit(Long id) {
+        Subreddit subreddit = subredditRepository.findById(id)
+                .orElseThrow(() -> new JARedditException("No subreddit found with ID " + id));
+        return subredditMapper.mapSubredditToDto(subreddit);
     }
 
     @Transactional
@@ -34,21 +50,29 @@ public class SubredditService {
         subredditDto.setId(subreddit.getId());
         return subredditDto;
     }
-
-    @Transactional(readOnly = true)
-    public SubredditDto getSubreddit(Long id) {
-        Subreddit subreddit = subredditRepository.findById(id)
-                .orElseThrow(() -> new SubredditNotFoundException("Subreddit not found with id " + id));
-        return mapToDto(subreddit);
-    }
-
-    private SubredditDto mapToDto(Subreddit subreddit) {
-        return SubredditDto.builder().name(subreddit.getName())
-                .id(subreddit.getId())
-                .postCount(subreddit.getPosts().size())
-                .build();
-    }
-
+//
+//    @Transactional(readOnly = true)
+//    public List<SubredditDto> getAll() {
+//        return subredditRepository.findAll()
+//                .stream()
+//                .map(this::mapToDto)
+//                .collect(toList());
+//    }
+//
+//    @Transactional(readOnly = true)
+//    public SubredditDto getSubreddit(Long id) {
+//        Subreddit subreddit = subredditRepository.findById(id)
+//                .orElseThrow(() -> new SubredditNotFoundException("Subreddit not found with id " + id));
+//        return mapToDto(subreddit);
+//    }
+//
+//    private SubredditDto mapToDto(Subreddit subreddit) {
+//        return SubredditDto.builder().name(subreddit.getName())
+//                .id(subreddit.getId())
+//                .numberOfPosts(subreddit.getPosts().size())
+//                .build();
+//    }
+//
     private Subreddit mapToSubreddit(SubredditDto subredditDto) {
         return Subreddit.builder().name("/r/" + subredditDto.getName())
                 .description(subredditDto.getDescription())
